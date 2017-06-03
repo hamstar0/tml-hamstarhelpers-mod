18f93227c7c7f9343d206851b139e2c2879dfb8f
hamstar <webutek@gmail.com>
2017-06-02 17:38:34 -0700

    v1.0.1 * Expended 'SimpleMessage' to include sub text

--- DisplayHelpers/SimpleMessage.cs
+++ DisplayHelpers/SimpleMessage.cs
@@ -7,11 +7,13 @@ namespace HamstarHelpers.DisplayHelpers {
 	public static class SimpleMessage {
 		public static int MessageDuration = 0;
 		public static string Message = "";
+		public static string SubMessage = "";
 
 
-		public static void PostMessage( string msg, int duration ) {
+		public static void PostMessage( string msg, string submsg, int duration ) {
 			SimpleMessage.MessageDuration = duration;
 			SimpleMessage.Message = msg;
+			SimpleMessage.SubMessage = submsg;
 		}
 
 		public static void UpdateMessage() { // Called from an Update function
@@ -20,14 +22,26 @@ namespace HamstarHelpers.DisplayHelpers {
 		}
 
 		public static void DrawMessage( SpriteBatch sb ) { // Called from a Draw function
-			float scale = 2f;
-			Vector2 pos = new Vector2( Main.screenWidth / 2f, Main.screenHeight / 2f );
+			var pos = new Vector2( Main.screenWidth / 2f, Main.screenHeight / 2f );
+			var size = SimpleMessage.DrawMessageText( sb, SimpleMessage.Message, 2f, pos );
+
+			if( SimpleMessage.SubMessage != "" ) {
+				var subpos = new Vector2( Main.screenWidth / 2f, (Main.screenHeight / 2f) + size.Y );
+				SimpleMessage.DrawMessageText( sb, SimpleMessage.SubMessage, 1f, subpos );
+			}
+		}
+
+		private static Vector2 DrawMessageText( SpriteBatch sb, string msg, float scale, Vector2 pos ) {
 			Vector2 size = Main.fontItemStack.MeasureString( SimpleMessage.Message );
+			size.X *= scale;
+			size.Y *= scale;
 
-			pos.X -= (size.X * scale) / 2f;
-			pos.Y -= (size.Y * scale) * 2f;
+			pos.X -= size.X / 2f;
+			pos.Y -= size.Y * 2f;
 
-			sb.DrawString( Main.fontItemStack, SimpleMessage.Message, pos, Color.White, 0f, new Vector2(), scale, SpriteEffects.None, 1f );
+			sb.DrawString( Main.fontItemStack, msg, pos, Color.White, 0f, new Vector2(), scale, SpriteEffects.None, 1f );
+
+			return size;
 		}
 	}
 }
