4893e4c9205ebe25543faedf1248edbaab33bfa5
hamstar <webutek@gmail.com>
2017-10-20 05:13:20 -0700

    v1.1.4.1 * Improved XnaColorHelpers

--- XnaHelpers/XnaColorHelpers.cs
+++ XnaHelpers/XnaColorHelpers.cs
@@ -4,89 +4,110 @@ using System;
 
 namespace HamstarHelpers.XnaHelpers {
 	public static class XnaColorHelpers {
-		/*public static Color Add( Color color, float amt, bool also_alpha ) {
-			float high = also_alpha ? ( color.R > color.G ?
-					(color.R > color.B ? (color.R > color.A ? color.R : color.A) : (color.B > color.A ? color.B : color.A)) :
-					(color.G > color.B ? (color.G > color.A ? color.G : color.A) : (color.B > color.A ? color.B : color.A)) )
-				: ( color.R > color.G ?
-					(color.R > color.B ? color.R : color.B) :
-					(color.G > color.B ? color.G : color.B) );
-
-			float r_scale = (float)color.R / high;
-			float g_scale = (float)color.G / high;
-			float b_scale = (float)color.B / high;
-			float a_scale = (float)color.A / high;
-
-			color.R = (byte)MathHelper.Clamp( color.R + (amt * r_scale), 0, 255 );
-			color.G = (byte)MathHelper.Clamp( color.G + (amt * g_scale), 0, 255 );
-			color.B = (byte)MathHelper.Clamp( color.B + (amt * b_scale), 0, 255 );
-			if( also_alpha ) {
-				color.A = (byte)MathHelper.Clamp( color.A + (amt * a_scale), 0, 255 );
-			}
-
-			return color;
-		}*/	// <- plz return brain to owner!
-
 		public static Color Add( Color c1, int amt, bool also_alpha ) {
-			byte cr = (byte)Math.Min( 255, (int)c1.R + amt );
-			byte cg = (byte)Math.Min( 255, (int)c1.G + amt );
-			byte cb = (byte)Math.Min( 255, (int)c1.B + amt );
+			byte cr = (byte)MathHelper.Clamp( (int)c1.R + amt, 0, 255 );
+			byte cg = (byte)MathHelper.Clamp( (int)c1.G + amt, 0, 255 );
+			byte cb = (byte)MathHelper.Clamp( (int)c1.B + amt, 0, 255 );
 			byte ca = c1.A;
 
 			if( also_alpha ) {
-				ca = (byte)Math.Min( 255, (int)c1.A + amt );
+				ca = (byte)MathHelper.Clamp( (int)c1.A + amt, 0, 255 );
 			}
 
 			return new Color( cr, cg, cb, ca );
 		}
-
-
+		
 		public static Color Add( Color c1, Color c2, bool also_alpha ) {
-			float scale = (float)c2.A / 255f;
+			int r = (int)c1.R + (int)c2.R;
+			int g = (int)c1.G + (int)c2.G;
+			int b = (int)c1.B + (int)c2.B;
 
-			byte cr = (byte)Math.Min( 255, (float)c1.R + ((float)c2.R * scale));
-			byte cg = (byte)Math.Min( 255, (float)c1.G + ((float)c2.G * scale));
-			byte cb = (byte)Math.Min( 255, (float)c1.B + ((float)c2.B * scale));
+			byte cr = (byte)MathHelper.Clamp( r, 0, 255 );
+			byte cg = (byte)MathHelper.Clamp( g, 0, 255 );
+			byte cb = (byte)MathHelper.Clamp( b, 0, 255 );
 			byte ca = c1.A;
 
 			if( also_alpha ) {
-				ca += (byte)( (float)(255 - ca) * scale );
+				int a = (int)c1.A + (int)c2.A;
+				ca = (byte)MathHelper.Clamp( a, 0, 255 );
 			}
 
 			return new Color( cr, cg, cb, ca );
 		}
 
-		public static Color Mul( Color color, float amt ) {
-			color.R = (byte)MathHelper.Clamp( (float)color.R * amt, 0, 255 );
-			color.G = (byte)MathHelper.Clamp( (float)color.G * amt, 0, 255 );
-			color.B = (byte)MathHelper.Clamp( (float)color.B * amt, 0, 255 );
-			color.A = (byte)MathHelper.Clamp( (float)color.A * amt, 0, 255 );
-			return color;
-		}
 
 
+		[System.Obsolete( "use XnaColorHelpers.Mul( Color c1, Color c2 )", true )]
 		public static Color BlendInto( Color to, Color from ) {
-			float r = (float)from.R / 255f;
-			float g = (float)from.G / 255f;
-			float b = (float)from.B / 255f;
-			float a = (float)from.A / 255f;
-			float na = 1f - a;
-			
-			to.R = (byte)Math.Min( 255, ((float)to.R * na) + (r * a) );
-			to.G = (byte)Math.Min( 255, ((float)to.G * na) + (g * a) );
-			to.B = (byte)Math.Min( 255, ((float)to.B * na) + (b * a) );
+			return XnaColorHelpers.Mul( to, from );
+		}
+		
+		[System.Obsolete( "use Color.Multiply( Color value, float scale )", true )]
+		public static Color Mul( Color value, float scale ) {
+			return Color.Multiply( value, scale );
+		}
+		
+		public static Color Mul( Color c1, Color c2 ) {
+			byte r = (byte)( (float)c1.R * ((float)c2.R / 255f) );
+			byte g = (byte)( (float)c1.G * ((float)c2.G / 255f) );
+			byte b = (byte)( (float)c1.B * ((float)c2.B / 255f) );
+			byte a = (byte)( (float)c1.A * ((float)c2.A / 255f) );
 
-			return to;
+			return new Color( r, g, b, a );
 		}
 
 
-		public static float Brightness( Color c, bool also_alpha ) {
-			float scale = (float)c.A / 255f;
-			float r = (float)c.R * scale;
-			float g = (float)c.G * scale;
-			float b = (float)c.B * scale;
-			float a = also_alpha ? scale : 1f;
-			return ( (0.299f * r) + (0.587f * g) + (0.114f * b) ) * a;
+		/*public static Color BlendIntoByAlpha( Color c1, Color c2 ) {
+			float scale = (float)c2.A / 255f;
+			float nscale = 1f - scale;
+
+			byte r = (byte)Math.Min( 255, ((float)c1.R * nscale) + ((float)c2.R * scale) );
+			byte g = (byte)Math.Min( 255, ((float)c1.G * nscale) + ((float)c2.G * scale) );
+			byte b = (byte)Math.Min( 255, ((float)c1.B * nscale) + ((float)c2.B * scale) );
+			//byte a = (byte)Math.Min( 255, (((int)to.A + (int)from.A) / 2) );
+			byte a = c1.A;
+
+			return new Color( r, g, b, a );
+		}*/
+
+
+		/*public static Color ShiftColors( Color color, float amt, bool also_alpha ) {
+			//float scale = XnaColorHelpers.Brightness( color, also_alpha ) * 255f;
+			float scale = (float)color.R + (float)color.G + (float)color.B;
+			if( also_alpha ) { scale = (scale + (float)color.A) / 4f; }
+			else { scale /= 3f; }
+
+			if( scale == 0 ) { return color; }
+
+			float r_scale = (float)color.R / scale;
+			float g_scale = (float)color.G / scale;
+			float b_scale = (float)color.B / scale;
+
+			float r = (float)color.R + (amt * r_scale);
+			float g = (float)color.G + (amt * g_scale);
+			float b = (float)color.B + (amt * b_scale);
+
+			color.R = (byte)MathHelper.Clamp( r, 0f, 255f );
+			color.G = (byte)MathHelper.Clamp( g, 0f, 255f );
+			color.B = (byte)MathHelper.Clamp( b, 0f, 255f );
+
+			if( also_alpha ) {
+				float a_scale = (float)color.A / scale;
+				float a = (float)color.A + (amt * a_scale);
+
+				color.A = (byte)MathHelper.Clamp( a, 0f, 255f );
+			}
+
+			return color;
+		}*/
+
+
+		public static float Brightness( Color c ) {
+			float r = (float)c.R / 255f;
+			float g = (float)c.G / 255f;
+			float b = (float)c.B / 255f;
+
+			return (0.299f * r) + (0.587f * g) + (0.114f * b);
 			//return (r + r + r + b + g + g + g + g) >> 3;
 		}
 	}
