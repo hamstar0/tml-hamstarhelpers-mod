4337c69d88b4d8c914dbc514877755fc6a166bcc
hamstar0 <webutek@gmail.com>
2018-04-28 09:27:48 -0700

    v1.4.5.1 * Fixed minor API issue with WorldHelpers.GetUniqueId()

--- Helpers/WorldHelpers/WorldHelpers.cs
+++ Helpers/WorldHelpers/WorldHelpers.cs
@@ -10,7 +10,11 @@ using Terraria.ModLoader.IO;
 
 namespace HamstarHelpers.WorldHelpers {
 	public partial class WorldHelpers {
-		public static string GetUniqueId( bool as_file_name=false ) {
+		public static string GetUniqueId() {
+			return WorldHelpers.GetUniqueId( false );
+		}
+
+		public static string GetUniqueId( bool as_file_name ) {
 			if( as_file_name ) {
 				return FileHelpers.SanitizePath( Main.worldName ) + "@" + Main.worldID;
 			} else {
