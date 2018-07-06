9e387e8102495adae1225825ad87e399c802e0f3
hamstar0 <webutek@gmail.com>
2018-07-06 08:05:36 -0700

    v2.0.0 * Imported PlayerModHelpers from v1.6.5.2 * Implemented rudimentary ClingsToRailEntityProperty

--- Components/CustomEntity/MovementProperties.cs
+++ Components/CustomEntity/MovementProperties.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
+using System;
 using Terraria;
+using Terraria.ID;
 
 
 namespace HamstarHelpers.Components.CustomEntity {
@@ -168,6 +170,36 @@ namespace HamstarHelpers.Components.CustomEntity {
 
 	public class ClingsToRailEntityProperty : CustomEntityProperty {
 		public override void Update( CustomEntity ent ) {
+			int x_beg = (int)ent.position.X / 16;
+			int y_beg = ( (int)ent.position.Y + ent.height ) / 16;
+
+			int x_end = Math.Max( ( (int)ent.position.X + ent.width ) / 16, x_beg + 1 );
+
+			int x_mid = ( x_beg + x_end ) / 2;
+
+			for( int i_off = 0; i_off < 2; i_off++ ) {
+				int flip = 0;
+
+				do {
+					int i = x_mid + i_off;
+
+					for( int j = y_beg; j < y_beg + 2; j++ ) {
+						if( Main.tile[i, j] != null && Main.tile[i, j].type == TileID.MinecartTrack ) {
+							this.SnapToTrack( ent, i, j );
+						}
+					}
+
+					if( i_off == 0 ) { break; }
+					i_off = -i_off;
+				} while( flip++ == 0 );
+			}
+		}
+
+
+		public void SnapToTrack( CustomEntity ent, int tile_x, int tile_y ) {
+			ent.position.Y = (tile_y * 16) - ent.height;
+			ent.velocity.X = 0;
+			ent.velocity.Y = 0;
 		}
 	}
 }
---
+++ Helpers/PlayerHelpers/PlayerModHelpers.cs
@@ -0,0 +1,42 @@
+﻿using HamstarHelpers.Helpers.DebugHelpers;
+using HamstarHelpers.Helpers.DotNetHelpers;
+using Terraria;
+using Terraria.ModLoader;
+
+
+namespace HamstarHelpers.Helpers.PlayerHelpers {
+	public static class PlayerModHelpers {
+		private static void RemoveWingSlotProperty( ModPlayer mywingplayer, string prop_name ) {
+			bool success;
+			object wing_equip_slot = ReflectionHelpers.GetField( mywingplayer, prop_name, out success );
+
+			if( success && wing_equip_slot != null ) {
+				Item wing_item = (Item)ReflectionHelpers.GetProperty( wing_equip_slot, "Item", out success );
+
+				if( success ) {
+					if( wing_item != null && !wing_item.IsAir ) {
+						ReflectionHelpers.SetProperty( wing_equip_slot, "Item", new Item(), out success );
+						ReflectionHelpers.SetField( mywingplayer, prop_name, wing_equip_slot, out success );
+					}
+				} else {
+					LogHelpers.Log( "Invalid Wing Mod item slot for " + prop_name );
+				}
+			} else {
+				LogHelpers.Log( "No Wing Mod item slot recognized for " + prop_name );
+			}
+		}
+
+
+		public static void ModdedExtensionsReset( Player player ) {
+			var wing_mod = ModLoader.GetMod( "WingSlot" );
+
+			if( wing_mod != null ) {
+				ModPlayer mywingplayer = player.GetModPlayer( wing_mod, "WingSlotPlayer" );
+
+				PlayerModHelpers.RemoveWingSlotProperty( mywingplayer, "EquipSlot" );
+				PlayerModHelpers.RemoveWingSlotProperty( mywingplayer, "VanitySlot" );
+				PlayerModHelpers.RemoveWingSlotProperty( mywingplayer, "DyeSlot" );
+			}
+		}
+	}
+}
