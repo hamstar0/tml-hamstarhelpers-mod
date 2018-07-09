95f7f2b450d62822a40dbe2314b1e4318d825100
hamstar0 <webutek@gmail.com>
2018-07-09 16:11:55 -0700

    v2.0.0 * Begun addiing PeriodicSync propertiy * Renamed property source files

---
+++ Components/CustomEntity/EntityProperties/PeriodicSync.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace HamstarHelpers.Components.CustomEntity.EntityProperties {
+	public class PeriodicSyncEntityProperty : CustomEntityProperty {
+		public override void Update( CustomEntity ent ) {
+			throw new NotImplementedException();
+		}
+	}
+}
