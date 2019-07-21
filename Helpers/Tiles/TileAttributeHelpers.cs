5047d20ab5d97d68f5c056eb74f66c6b766f35d8
hamstar0 <webutek@gmail.com>
2019-07-21 00:13:43 -0700

    v5.0.0 * Changed TileIdentityHelpers into TileAttributeHelpers * Split off GetReadableUniqueKey(...) into TileIdentityHelpers * Finished implementing TileWallIdentityHelpers

---
+++ Helpers/Tiles/TileAttributeHelpers.cs
@@ -0,0 +1,24 @@
+﻿using HamstarHelpers.Helpers.Debug;
+using System;
+using Terraria;
+
+
+namespace HamstarHelpers.Helpers.Tiles {
+	/// <summary>
+	/// Assorted static "helper" functions pertaining to tile attributes.
+	/// </summary>
+	public partial class TileAttributeHelpers {
+		/// <summary>
+		/// Indicates if a given tile type is an "object" (container, sign, station, etc.).
+		/// </summary>
+		/// <param name="tileType"></param>
+		/// <returns></returns>
+		public static bool IsObject( int tileType ) {
+			return Main.tileFrameImportant[tileType]
+				|| Main.tileContainer[tileType]
+				|| Main.tileSign[tileType]
+				|| Main.tileAlch[tileType]
+				|| Main.tileTable[tileType]; //tileFlame
+		}
+	}
+}
--- Helpers/Tiles/TileWallIdentityHelpers.cs
+++ Helpers/Tiles/TileWallIdentityHelpers.cs
@@ -1,23 +1,43 @@
-﻿using System;
+﻿using ReLogic.Reflection;
+using System;
+using Terraria.ID;
+using Terraria.ModLoader;
 
 
 namespace HamstarHelpers.Helpers.Tiles {
+	/// <summary>
+	/// Assorted static "helper" functions pertaining to wall identification.
+	/// </summary>
 	public class TileWallIdentityHelpers {
-		public static readonly IdDictionary Search = IdDictionary.Create<WallID, short>();
+		private static readonly IdDictionary Search = IdDictionary.Create<WallID, short>();
 
+
+
+		////////////////
+
+		/// <summary>
+		/// Gets the readable unique key for a given wall (e.g. for ModConfig fields).
+		/// </summary>
+		/// <param name="wallType"></param>
+		/// <returns></returns>
 		public static string GetReadableUniqueKey( int wallType ) {
-			if( WallID.Search.ContainsId( wallType ) ) {
-				return "Terraria " + WallID.Search.GetName( wallType );
+			if( TileWallIdentityHelpers.Search.ContainsId( wallType ) ) {
+				return "Terraria " + TileWallIdentityHelpers.Search.GetName( wallType );
 			}
 
 			ModWall modWall = WallLoader.GetWall( wallType );
 			if( modWall != null ) {
-				return WallID.GetReadableUniqueKey( modWall );
+				return TileWallIdentityHelpers.GetReadableUniqueKey( modWall );
 			}
 
 			throw new ArgumentOutOfRangeException( "Invalid wall type value." );
 		}
 
+		/// <summary>
+		/// Gets the readable unique key for a given wall (e.g. for ModConfig fields).
+		/// </summary>
+		/// <param name="wall"></param>
+		/// <returns></returns>
 		public static string GetReadableUniqueKey( ModWall wall ) {
 			return wall.mod.Name + " " + wall.Name;
 		}
