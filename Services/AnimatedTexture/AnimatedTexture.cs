2dd21fc912cb67ccd1ca23822f5ee411f78099d6
hamstar0 <webutek@gmail.com>
2019-09-10 20:58:23 -0700

    v5.1.0 - AnimatedTexture uses "Animator" * Modified AnimatedTexture to use animator function

--- Services/AnimatedTexture/AnimatedTexture.cs
+++ Services/AnimatedTexture/AnimatedTexture.cs
@@ -1,24 +1,25 @@
-﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework.Graphics;
 using System;
 using Terraria;
 
 
 namespace HamstarHelpers.Services.AnimatedTexture {
 	/// <summary>
-	/// Supplies a handy way to "animate" (lerp between) colors to make animations. Adjustable.
+	/// Supplies a handy way to animate between frames within a given texture. Adjustable.
 	/// </summary>
 	public partial class AnimatedTexture {
 		/// <summary>
-		/// Creates an animated color object.
+		/// Creates an animated texture object.
 		/// </summary>
 		/// <param name="frames"></param>
-		/// <param name="durationPerFrame">Ticks of time to spend between each frame.</param>
-		/// <param name="maxFrames">Number of frames to cycle through.</param>
+		/// <param name="maxFrames">Ticks of time to spend between each frame.</param>
+		/// <param name="animator">Number of frames to cycle through.</param>
 		/// <returns></returns>
-		public static AnimatedTexture Create( Texture2D frames, int durationPerFrame, int maxFrames ) {
+		public static AnimatedTexture Create( Texture2D frames,
+				int maxFrames,
+				Func<AnimatedTexture, (int NextFrame, int TickDelay)> animator ) {
 			AnimatedTextureManager mngr = ModHelpersMod.Instance.AnimatedTextures;
-			var def = new AnimatedTexture( frames, durationPerFrame, maxFrames );
+			var def = new AnimatedTexture( frames, maxFrames, animator );
 
 			mngr.AddAnimation( def );
 
