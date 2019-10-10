d03e3e55157ca181d024a02328afa71484c3c4ac
hamstar0 <webutek@gmail.com>
2019-10-10 03:08:00 -0700

    v5.2.0 - Added BTree (WIP)

---
+++ Classes/DataStructures/BTree/BTreeEntry.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace HamstarHelpers.Classes.DataStructures.BTree {
+	/// <summary></summary>
+	/// <typeparam name="TK"></typeparam>
+	/// <typeparam name="TP"></typeparam>
+	public class BTreeEntry<TK, TP> : IEquatable<BTreeEntry<TK, TP>> {
+		/// <summary></summary>
+		public TK Key { get; set; }
+
+		/// <summary></summary>
+		public TP Pointer { get; set; }
+
+
+
+		/// <summary></summary>
+		/// <param name="other"></param>
+		/// <returns></returns>
+		public bool Equals( BTreeEntry<TK, TP> other ) {
+			return this.Key.Equals( other.Key ) && this.Pointer.Equals( other.Pointer );
+		}
+	}
+}
---
+++ Classes/DataStructures/BTree/BTreeNode.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+
+namespace HamstarHelpers.Classes.DataStructures.BTree {
+	/// <summary></summary>
+	/// <typeparam name="TK"></typeparam>
+	/// <typeparam name="TP"></typeparam>
+	public class BTreeNode<TK, TP>  {
+        private int Degree;
+
+
+
+		/// <summary></summary>
+		public List<BTreeNode<TK, TP>> Children { get; set; }
+
+		/// <summary></summary>
+		public List<BTreeEntry<TK, TP>> Entries { get; set; }
+
+		/// <summary></summary>
+		public bool IsLeaf {
+			get {
+				return this.Children.Count == 0;
+			}
+		}
+
+		/// <summary></summary>
+		public bool HasReachedMaxEntries {
+			get {
+				return this.Entries.Count == ( 2 * this.Degree ) - 1;
+			}
+		}
+
+		/// <summary></summary>
+		public bool HasReachedMinEntries {
+			get {
+				return this.Entries.Count == this.Degree - 1;
+			}
+		}
+
+
+
+		/// <summary></summary>
+		/// <param name="degree"></param>
+		public BTreeNode( int degree ) {
+			this.Degree = degree;
+			this.Children = new List<BTreeNode<TK, TP>>( degree );
+			this.Entries = new List<BTreeEntry<TK, TP>>( degree );
+		}
+	}
+}
