df627a76d6c5cd98ea73a0007b8e94184b53ffa9
hamstar0 <webutek@gmail.com>
2019-10-30 02:03:50 -0700

    v5.3.1 * Added TileFinderHelpers.GetAllContiguousMatchingTiles(...)

--- Helpers/Tiles/TileFinderHelpers_FindContiguous.cs
+++ Helpers/Tiles/TileFinderHelpers_FindContiguous.cs
@@ -1,6 +1,8 @@
 ﻿using HamstarHelpers.Classes.Tiles.TilePattern;
 using HamstarHelpers.Helpers.Debug;
+using HamstarHelpers.Helpers.DotNET;
 using System;
+using System.Collections.Generic;
 using Terraria;
 
 
@@ -51,6 +53,85 @@ namespace HamstarHelpers.Helpers.Tiles {
 
 			coords = ( TileX: tileX - maxX, TileY: tileY - maxY);
 			return foundX && foundY;
+		}
+
+
+		/// <summary>
+		/// Gets a list of all contiguous tiles matching the given pattern.
+		/// </summary>
+		/// <param name="pattern"></param>
+		/// <param name="tileX"></param>
+		/// <param name="tileY"></param>
+		/// <param name="maxRadius"></param>
+		/// <param name="maxTiles"></param>
+		/// <returns></returns>
+		public static IEnumerable<(ushort TileX, ushort TileY)> GetAllContiguousMatchingTiles(
+					TilePattern pattern,
+					int tileX,
+					int tileY,
+					int maxRadius = -1,
+					int maxTiles = -1 ) {
+			if( !pattern.Check(tileX, tileY) ) {
+				return new (ushort, ushort)[ 0 ];
+			}
+
+			ISet<int> unchartedTileMap = new HashSet<int>{ tileX + (tileY<<16) };
+			ISet<int> newUnchartedTileMap = new HashSet<int>();
+			ISet<int> chartedTileMap = new HashSet<int>();
+
+			int maxRadiusSqr = maxRadius * maxRadius;
+			int distX, distY, distSqr;
+
+			do {
+				foreach( int tileAt in unchartedTileMap ) {
+					int x = (tileAt << 16) >> 16;
+					int y = tileAt >> 16;
+
+					if( maxTiles > 0 && chartedTileMap.Count >= maxTiles ) {
+						break;
+					}
+
+					if( maxRadius > 0 ) {
+						distX = x - tileX;
+						distY = y - tileY;
+						distSqr = (distX * distX) + (distY * distY);
+
+						if( distSqr >= maxRadiusSqr ) {
+							continue;
+						}
+					}
+
+					if( pattern.Check(x, y) ) {
+						chartedTileMap.Add( tileAt );
+						newUnchartedTileMap.Add( x + ((y-1)<<16) );
+						newUnchartedTileMap.Add( (x-1) + (y<<16) );
+						newUnchartedTileMap.Add( (x+1) + (y<<16) );
+						newUnchartedTileMap.Add( x + ((y+1)<<16) );
+					}
+				}
+
+				if( maxTiles > 0 && chartedTileMap.Count >= maxTiles ) {
+					break;
+				}
+
+				unchartedTileMap.Clear();
+				foreach( int tileAt in newUnchartedTileMap ) {
+					if( chartedTileMap.Contains(tileAt) ) {
+						continue;
+					}
+					unchartedTileMap.Add( tileAt );
+				}
+
+				newUnchartedTileMap.Clear();
+			} while( unchartedTileMap.Count > 0 );
+
+			return chartedTileMap
+				.SafeSelect(
+					tileAt => (
+						(ushort)((tileAt<<16)>>16),
+						(ushort)(tileAt>>16)
+					)
+				);
 		}
 	}
 }
