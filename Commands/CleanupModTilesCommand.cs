0f55c8ef6e9141d93c7ab697468ed2fbbb45fd5f
hamstar0 <webutek@gmail.com>
2019-11-04 08:13:49 -0800

    v5.3.2 - Fixed multithreading for mod tiles cleanup (untested)

--- Commands/CleanupModTilesCommand.cs
+++ Commands/CleanupModTilesCommand.cs
@@ -14,37 +14,84 @@ using Terraria.ModLoader.Default;
 namespace HamstarHelpers.Commands {
 	/// @private
 	public class CleanupModTilesCommand : ModCommand {
+		private static object MyLock = new object();
+
+
+
 		/// <summary>
 		/// </summary>
 		public static void CleanupAsync( Action<int> onCompletion ) {
 			Task.Run( () => {
 				int cleaned = 0;
-				
-				//for( int i = 0; i < Main.maxTilesX; i++ ) {
-				Parallel.For( 0, Main.maxTilesX, ( i ) => {
-					for( int j = 0; j < Main.maxTilesY; j++ ) {
-						Tile tile = Framing.GetTileSafely( i, j );
-						// No air or vanilla tiles
-						if( !tile.active() || TileID.Search.ContainsId( tile.type ) ) { continue; }
 
-						ModTile modTile = ModContent.GetModTile( tile.type );
-						if( modTile == null ) { continue; }
+				var cts = new CancellationTokenSource();
+				var options = new ParallelOptions {
+					CancellationToken = cts.Token
+				};
 
-						if( modTile.mod == null || modTile is MysteryTile ) {
-							TileHelpers.KillTile( i, j, false, false );
-							cleaned++;
-						}
+				try {
+					//for( int i = 0; i < Main.maxTilesX; i++ ) {
+					Parallel.For( 0, Main.maxTilesX, options, ( i ) => {
+						for( int j = 0; j < Main.maxTilesY; j++ ) {
+							if( options.CancellationToken.IsCancellationRequested ) {
+								return;
+							}
 
-						if( Main.netMode != 0 ) {
-							if( cleaned % 100 == 0 && cleaned > 0 ) {
-								Thread.Sleep( 1500 );	// 1.5s cooldowns to not flood the server
+							if( CleanupModTilesCommand.CleanupTile( i, j, cts ) ) {
+								lock( CleanupModTilesCommand.MyLock ) {
+									cleaned++;
+
+									// To prevent hammering the server
+									if( Main.netMode != 0 ) {
+										if( cleaned % 100 == 0 ) {
+											Thread.Sleep( 500 );
+										}
+									}
+								}
 							}
 						}
-					}
-				} );
+					} );
+				} finally {
+					cts.Dispose();
+				}
 
 				onCompletion( cleaned );
 			} );
+		}
+
+
+		private static bool CleanupTile( int i, int j, CancellationTokenSource cts ) {
+			Tile tile = Main.tile[i, j];
+			if( tile == null ) {
+				lock( CleanupModTilesCommand.MyLock ) {
+					tile = Framing.GetTileSafely( i, j );
+				}
+			}
+
+			// No air or vanilla tiles
+			if( !tile.active() || TileID.Search.ContainsId( tile.type ) ) {
+				return false;
+			}
+
+			ModTile modTile = ModContent.GetModTile( tile.type );
+			if( modTile == null ) {
+				return false;
+			}
+
+			if( modTile.mod != null && !(modTile is MysteryTile) ) {
+				return false;
+			}
+
+			try {
+				lock( CleanupModTilesCommand.MyLock ) {
+					TileHelpers.KillTile( i, j, false, false );
+				}
+			} catch {
+				cts.Cancel();
+				return false;
+			}
+
+			return true;
 		}
 
 
