5168d9d184e1c5722c290bdf34a50a44d9d749f0
hamstar0 <webutek@gmail.com>
2019-11-04 12:02:24 -0800

    v5.3.2 - Fixed CustomPlayerData.OnEnter MP fail

--- Classes/CustomPlayerData/CustomPlayerData_Internal.cs
+++ Classes/CustomPlayerData/CustomPlayerData_Internal.cs
@@ -1,4 +1,5 @@
 ﻿using HamstarHelpers.Classes.Loadable;
+using HamstarHelpers.Helpers.Debug;
 using HamstarHelpers.Helpers.DotNET.Extensions;
 using HamstarHelpers.Helpers.DotNET.Reflection;
 using HamstarHelpers.Helpers.Players;
@@ -14,9 +15,13 @@ namespace HamstarHelpers.Classes.PlayerData {
 	/// An alternative to ModPlayer for basic per-player, per-game data storage and Update use.
 	/// </summary>
 	public partial class CustomPlayerData : ILoadable {
-		private static void InitializeAll( int playerWho ) {
+		private static void Enter( int playerWho ) {
+			if( ModHelpersMod.Config.DebugModeHelpersInfo ) {
+				LogHelpers.Alert( "Player "+Main.player[playerWho].name+" ("+playerWho+") entered the game." );
+			}
+
 			CustomPlayerData singleton = ModContent.GetInstance<CustomPlayerData>();
-			IEnumerable<Type> plrDataTypes = ReflectionHelpers.GetAllAvailableSubTypesFromMods( typeof(CustomPlayerData) );
+			IEnumerable<Type> plrDataTypes = ReflectionHelpers.GetAllAvailableSubTypesFromMods( typeof( CustomPlayerData ) );
 
 			foreach( Type plrDataType in plrDataTypes ) {
 				object data = CustomPlayerData.LoadFileData( plrDataType.Name, PlayerIdentityHelpers.GetUniqueId() );
@@ -34,33 +39,40 @@ namespace HamstarHelpers.Classes.PlayerData {
 			}
 		}
 
+		private static void Exit( int playerWho ) {
+			CustomPlayerData singleton = ModContent.GetInstance<CustomPlayerData>();
 
-		////
+			foreach( CustomPlayerData plrData in singleton.DataMap[playerWho] ) {
+				object data = plrData.OnExit();
+				if( data != null ) {
+					CustomPlayerData.SaveFileData( plrData.GetType().Name, PlayerIdentityHelpers.GetUniqueId(), data );
+				}
+			}
+		}
+
+
+		////////////////
 
 		private static void UpdateAll() {
-			bool isInGame = !Main.gameMenu && LoadHelpers.IsWorldLoaded();
+			bool isInGame = !Main.gameMenu && LoadHelpers.IsWorldBeingPlayed();
 
 			CustomPlayerData singleton = ModContent.GetInstance<CustomPlayerData>();
 			Player player;
 
-			for( int i=0; i<Main.maxPlayers; i++ ) {
+			for( int i = 0; i < Main.maxPlayers; i++ ) {
 				player = Main.player[i];
 
 				if( player == null || !player.active ) {
-					if( singleton.DataMap.ContainsKey(i) ) {
-						foreach( CustomPlayerData plrData in singleton.DataMap[i] ) {
-							object data = plrData.OnExit();
-
-							CustomPlayerData.SaveFileData( plrData.GetType().Name, PlayerIdentityHelpers.GetUniqueId(), data );
-						}
+					if( singleton.DataMap.ContainsKey( i ) ) {
+						CustomPlayerData.Exit( i );
 					}
-					
+
 					continue;
 				}
 
 				if( isInGame ) {
 					if( !singleton.DataMap.ContainsKey( i ) ) {
-						CustomPlayerData.InitializeAll( i );
+						CustomPlayerData.Enter( i );
 					} else {
 						foreach( CustomPlayerData plrData in singleton.DataMap[i] ) {
 							plrData.Update();
@@ -68,9 +80,7 @@ namespace HamstarHelpers.Classes.PlayerData {
 					}
 				} else {
 					if( singleton.DataMap.ContainsKey( i ) ) {
-						foreach( CustomPlayerData plrData in singleton.DataMap[i] ) {
-							plrData.OnExit();
-						}
+						CustomPlayerData.Exit( i );
 					}
 				}
 			}
