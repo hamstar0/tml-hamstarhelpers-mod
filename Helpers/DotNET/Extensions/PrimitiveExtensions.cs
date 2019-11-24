0036e4cf9fa08c3741a3ed00660bc52052114d96
hamstar0 <webutek@gmail.com>
2019-11-23 22:22:41 -0800

    v5.4.0 - Added PrimitiveExtensions

---
+++ Helpers/DotNET/Extensions/PrimitiveExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace HamstarHelpers.Helpers.DotNET.Extensions {
+	/// <summary>
+	/// Extensions for general types.
+	/// </summary>
+	public static class PrimitiveExtensions {
+		/// <summary>
+		/// Clamps most primitive value types.
+		/// Credit to absoluteAquarian
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="value"></param>
+		/// <param name="min"></param>
+		/// <param name="max"></param>
+		public static void Clamp<T>( this ref T value, T min, T max ) where T : struct, IComparable<T> {
+			value = value.CompareTo( max ) > 0
+				? max
+				: ( value.CompareTo( min ) < 0
+					? min
+					: value );
+			int blah = 0;
+		}
+	}
+}
