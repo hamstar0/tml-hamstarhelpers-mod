0f24b05733697c25f94142b9d3b073280758ac37
hamstar0 <webutek@gmail.com>
2019-12-30 20:11:52 -0800

    v5.4.0 - Tweaked path finding algorithm

--- Helpers/Collisions/TileCollisionHelpers.cs
+++ Helpers/Collisions/TileCollisionHelpers.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using HamstarHelpers.Services.Timers;
+using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -64,19 +65,19 @@ namespace HamstarHelpers.Helpers.Collisions {
 
 			int getDistSqr( (int x, int y) a, (int x, int y) b ) {
 				int diffX = a.x - b.x;
-				int diffY = a.y - b.x;
+				int diffY = a.y - b.y;
 				return (diffX * diffX) + (diffY * diffY);
 			}
 
 			bool tryGetDistSqrOfValidTile( (int x, int y) point, out int distSqr ) {
-				distSqr = -1;
+				distSqr = (Main.maxTilesX * Main.maxTilesX) + (Main.maxTilesY * Main.maxTilesY);
 
 				if( point.x < 0 || point.x >= Main.maxTilesX ) { return false; }
 				if( point.y < 0 || point.y >= Main.maxTilesY ) { return false; }
 				if( chartedTiles.Contains(point) ) { return false; }
 
 				Tile tile = Framing.GetTileSafely( point.x, point.y );
-				if( tile.active() ) { return false; }
+				if( tile.active() && Main.tileSolid[tile.type] ) { return false; }
 
 				distSqr = getDistSqr( point, tilePointB );
 				return true;
@@ -120,27 +121,28 @@ namespace HamstarHelpers.Helpers.Collisions {
 				}
 			}
 
-			bool tryGetNeighborClosestToTarget( (int x, int y) pt, out (int x, int y) closest ) {
-				(int x, int y, int distSqr) prev = (0, 0, Main.maxTilesX * Main.maxTilesX);
-				(int x, int y, int distSqr) closestDist = getDistSqrNeighborsNear( pt )
-					.Single( curr => {
-						bool isSmallest = curr.distSqr < prev.distSqr;
-						prev = curr;
-						return isSmallest;
-					} );
+			bool tryGetNeighborClosestToTarget( (int x, int y) point, out (int x, int y) closestNeighborToTarget ) {
+				(int x, int y, int distSqr) prev = (-1, -1, Main.maxTilesX * Main.maxTilesX);
+				IEnumerable<(int x, int y, int distSqr)> validNeighbors = getDistSqrNeighborsNear( point );
 
-				closest = (closestDist.x, closestDist.y);
-				return closestDist.x != 0;
+				foreach( (int x, int y, int distSqr) validNeighbor in validNeighbors ) {
+					if( validNeighbor.distSqr < prev.distSqr ) {
+						prev = validNeighbor;
+					}
+				}
+
+				closestNeighborToTarget = (prev.x, prev.y);
+				return closestNeighborToTarget.x != -1;
 			}
 
 			//
 
 			(int x, int y) currPt = tilePointA;
 
-			do {
-				(int x, int y) closestNeighbor;
+			while( currPt.x != tilePointB.x || currPt.y != tilePointB.y ) {
+				(int x, int y) closestNeighborToTarget;
 
-				if( !tryGetNeighborClosestToTarget(currPt, out closestNeighbor) ) {
+				if( !tryGetNeighborClosestToTarget(currPt, out closestNeighborToTarget) ) {
 					if( path.Count == 0 ) {
 						return false;
 					}
@@ -158,9 +160,17 @@ namespace HamstarHelpers.Helpers.Collisions {
 				path.Add( currPt );
 				chartedTiles.Add( currPt );
 
-				currPt = closestNeighbor;
-			} while( currPt != tilePointB );
+				currPt = closestNeighborToTarget;
+			}
 
+/*int blah = 120;
+var pathCopy = path.ToList();
+Timers.SetTimer( "blah_"+tilePointA.x+"_"+tilePointA.y, 4, false, () => {
+	foreach( (int x, int y) node in pathCopy.ToList() ) {
+		Dust.QuickDust( new Point(node.x, node.y), Color.Red );
+	}
+	return blah-- > 0;
+} );*/
 			return true;
 		}
 	}
