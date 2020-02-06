5015daa68ed002d1de7185091f7bedbbfe00bbd8
hamstar0 <webutek@gmail.com>
2020-02-06 05:41:34 -0800

    v5.4.4 - Added HUDMapHelpers.GetFullscreenMapTileOfScreenPosition

--- Helpers/HUD/HudMapHelpers.cs
+++ Helpers/HUD/HudMapHelpers.cs
@@ -194,5 +194,40 @@ namespace HamstarHelpers.Helpers.HUD {
 
 			return mapNewPos - mapBasePos;
 		}
+
+
+		////////////////
+
+		/// <summary>
+		/// Outputs the world tile position of a given screen position in the fullscreen map.
+		/// </summary>
+		/// <param name="scrPosX"></param>
+		/// <param name="scrPosY"></param>
+		/// <param name="tilePos"></param>
+		/// <returns>`true` if the tile is within world bounds.</returns>
+		public static bool GetFullscreenMapTileOfScreenPosition( int scrPosX, int scrPosY, out (int x, int y) tilePos ) {
+			float mapScale = Main.mapFullscreenScale;
+			float minX = 10f;
+			float minY = 10f;
+			float maxX = (float)( Main.maxTilesX - 10 );
+			float maxY = (float)( Main.maxTilesY - 10 );
+
+			float mapPosX = Main.mapFullscreenPos.X * mapScale;
+			float mapPosY = Main.mapFullscreenPos.Y * mapScale;
+
+			float scrOriginX = (float)(Main.screenWidth / 2) - mapPosX;
+			scrOriginX += minX * mapScale;
+			float scrOriginY = (float)(Main.screenHeight / 2) - mapPosY;
+			scrOriginY += minX * mapScale;
+
+			int tileX = (int)(((float)scrPosX - scrOriginX) / mapScale + minX);
+			int tileY = (int)(((float)scrPosY - scrOriginY) / mapScale + minY);
+
+			tilePos = (tileX, tileY);
+			return tileX >= minX
+				&& tileX < maxX
+				&& tileY >= minY
+				&& tileY < maxY;
+		}
 	}
 }
