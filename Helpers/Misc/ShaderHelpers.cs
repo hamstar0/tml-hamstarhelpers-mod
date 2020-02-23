33d4f21e5935db2a8b0268401e813959d5de9e6d
hamstar0 <webutek@gmail.com>
2020-02-23 04:37:09 -0800

    v5.5.1 - Added ShaderHelpers (Misc)

---
+++ Helpers/Misc/ShaderHelpers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Terraria.Graphics.Shaders;
+
+
+namespace HamstarHelpers.Helpers.Misc {
+	/// <summary>
+	/// Assorted static "helper" functions pertaining to shaders.
+	/// </summary>
+	public class ShaderHelpers {
+		/// <summary>
+		/// Generalized getter for shader ids from item types.
+		/// </summary>
+		/// <param name="itemType"></param>
+		/// <returns>-1 if no shader id exists for the given item.</returns>
+		public static int GetShaderIdByDyeItemType( int itemType ) {
+			int shaderId = (int)GameShaders.Armor.GetShaderIdFromItemId( itemType );
+			if( shaderId > 0 ) {
+				return shaderId;
+			}
+			shaderId = (int)GameShaders.Hair.GetShaderIdFromItemId( itemType );
+			if( shaderId >= 0 ) {
+				return shaderId;
+			}
+			return -1;
+		}
+	}
+}
