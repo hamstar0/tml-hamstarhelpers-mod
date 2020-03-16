0d65c19d26c35cce9e86f04deaf57055606edb18
hamstar0 <webutek@gmail.com>
2020-03-16 13:21:00 -0700

    v5.5.1 - Missed a spot

--- Helpers/Players/PlayerHeadDrawHelpers.cs
+++ Helpers/Players/PlayerHeadDrawHelpers.cs
@@ -1,22 +1,16 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿using System;
+using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using HamstarHelpers.Helpers.DotNET.Reflection;
 
 
 namespace HamstarHelpers.Helpers.Players {
 	/// <summary>
-	/// Assorted static "helper" functions pertaining to player head drawing.
+	/// Assorted static "helper" functions pertaining to player head drawing (currently empty).
 	/// </summary>
 	public class PlayerHeadDrawHelpers {
-		/// <summary>
-		/// Draws a player's head.
-		/// </summary>
-		/// <param name="sb">SpriteBatch to draw to. Typically `Main.spriteBatch`.</param>
-		/// <param name="player"></param>
-		/// <param name="x"></param>
-		/// <param name="y"></param>
-		/// <param name="alpha"></param>
-		/// <param name="scale"></param>
+		/// @private
+		[Obsolete( "uses vanilla's Main.instance.DrawPlayerHead via. reflection; not library code", true)]
 		public static void DrawPlayerHead( SpriteBatch sb, Player player, float x, float y, float alpha = 1f, float scale = 1f ) {
 			object _;
 			ReflectionHelpers.RunMethod( Main.instance, "DrawPlayerHead", new object[] { player, x, y, alpha, scale }, out _ );
