6d27e9a2148d5aa77247f9032a737b85bda03d8c
hamstar0 <webutek@gmail.com>
2020-06-06 12:56:07 -0700

    v5.7.4 - Further restructured UIDialog code files (open/close stuff)

--- Classes/UI/Elements/UIDialog.cs
+++ Classes/UI/Elements/UIDialog.cs
@@ -4,7 +4,6 @@ using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.UI;
 using HamstarHelpers.Classes.UI.Theme;
-using HamstarHelpers.Internals.UI;
 
 
 namespace HamstarHelpers.Classes.UI.Elements {
@@ -43,7 +42,7 @@ namespace HamstarHelpers.Classes.UI.Elements {
 		private float LeftPercent = 0.5f;
 		private bool TopCentered = true;
 		private bool LeftCentered = true;
-		
+
 
 
 		////////////////
@@ -60,10 +59,7 @@ namespace HamstarHelpers.Classes.UI.Elements {
 
 		////////////////
 
-		/// <summary>
-		/// Updates state.
-		/// </summary>
-		/// <param name="gameTime">Unused.</param>
+		/// @private
 		public override void Update( GameTime gameTime ) {
 			base.Update( gameTime );
 
@@ -71,67 +67,11 @@ namespace HamstarHelpers.Classes.UI.Elements {
 				return;
 			}
 
-			if( Main.playerInventory || Main.npcChatText != "" || this.Backend == null || this.Backend.CurrentState != this ) {
-				this.Close();
-				return;
-			}
-			
-			if( this.SetDialogToClose ) {
-				this.SetDialogToClose = false;
-				this.Close();
-				return;
-			}
+			this.UpdateOpenState();
 
 			if( this.OuterContainer.IsMouseHovering ) {
 				Main.LocalPlayer.mouseInterface = true;
 			}
-		}
-
-
-		////////////////
-
-		/// <returns>`true` if dialog can be opened (UI not otherwise captured, no other dialogs, etc.).</returns>
-		public virtual bool CanOpen() {
-			return !this.IsOpen && !Main.inFancyUI &&
-				(DialogManager.Instance != null && DialogManager.Instance.CurrentDialog == null);
-		}
-
-
-		/// <summary>
-		/// Opens the dialog. All input and UI context is captured.
-		/// </summary>
-		public virtual void Open() {
-			this.IsOpen = true;
-
-			Main.playerInventory = false;
-			Main.editChest = false;
-			Main.npcChatText = "";
-
-			Main.inFancyUI = true;
-			Main.InGameUI.SetState( (UIState)this );
-
-			this.Backend = Main.InGameUI;
-
-			this.RecalculateMe();
-
-			if( DialogManager.Instance != null ) {
-				DialogManager.Instance.SetCurrentDialog( this );
-			}
-		}
-
-
-		/// <summary>
-		/// Closes the current dialog. All UI context is reverted to the game's normal state.
-		/// </summary>
-		public virtual void Close() {
-			this.IsOpen = false;
-
-			if( Main.InGameUI.CurrentState == this ) {
-				Main.inFancyUI = false;
-				Main.InGameUI.SetState( (UIState)null );
-			}
-
-			this.Backend = null;
 		}
 
 
---
+++ Classes/UI/Elements/UIDialog_Open.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Terraria;
+using Terraria.UI;
+using HamstarHelpers.Classes.UI.Theme;
+using HamstarHelpers.Internals.UI;
+
+
+namespace HamstarHelpers.Classes.UI.Elements {
+	/// <summary>
+	/// Defines a UI dialog (stand-alone, centered panel) element. All dialogs are modal, and exclusively capture all interactions until closed.
+	/// </summary>
+	public abstract partial class UIDialog : UIThemedState {
+		private void UpdateOpenState() {
+			if( Main.playerInventory || Main.npcChatText != "" || this.Backend == null || this.Backend.CurrentState != this ) {
+				this.Close();
+				return;
+			}
+
+			if( this.SetDialogToClose ) {
+				this.SetDialogToClose = false;
+				this.Close();
+				return;
+			}
+		}
+
+
+		////////////////
+
+		/// <returns>`true` if dialog can be opened (UI not otherwise captured, no other dialogs, etc.).</returns>
+		public virtual bool CanOpen() {
+			return !this.IsOpen && !Main.inFancyUI &&
+				(DialogManager.Instance != null && DialogManager.Instance.CurrentDialog == null);
+		}
+
+
+		/// <summary>
+		/// Opens the dialog. All input and UI context is captured.
+		/// </summary>
+		public virtual void Open() {
+			this.IsOpen = true;
+
+			Main.playerInventory = false;
+			Main.editChest = false;
+			Main.npcChatText = "";
+
+			Main.inFancyUI = true;
+			Main.InGameUI.SetState( (UIState)this );
+
+			this.Backend = Main.InGameUI;
+
+			this.RecalculateMe();
+
+			if( DialogManager.Instance != null ) {
+				DialogManager.Instance.SetCurrentDialog( this );
+			}
+		}
+
+
+		/// <summary>
+		/// Closes the current dialog. All UI context is reverted to the game's normal state.
+		/// </summary>
+		public virtual void Close() {
+			this.IsOpen = false;
+
+			if( Main.InGameUI.CurrentState == this ) {
+				Main.inFancyUI = false;
+				Main.InGameUI.SetState( (UIState)null );
+			}
+
+			this.Backend = null;
+		}
+	}
+}
