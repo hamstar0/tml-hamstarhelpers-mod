b46891a70f5b61d37e6923b3dba88d6707c366a1
hamstar0 <webutek@gmail.com>
2020-09-07 01:00:40 -0700

    v5.9.2 - Finished implementing PropNPC

--- HamstarHelpers/NPCs/PropNPC.cs
+++ HamstarHelpers/NPCs/PropNPC.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using HamstarHelpers.Helpers.Debug;
+using HamstarHelpers.Helpers.UI;
+using HamstarHelpers.Helpers.Draw;
 
 
 namespace HamstarHelpers.NPCs {
@@ -12,7 +17,7 @@ namespace HamstarHelpers.NPCs {
 		/// <summary></summary>
 		public override string Texture => this._Texture;
 
-		private string _Texture = "Terraria/MapDeath";
+		private string _Texture = "HamstarHelpers/NPCs/PropNPC";
 
 
 
@@ -20,14 +25,14 @@ namespace HamstarHelpers.NPCs {
 
 		/// @private
 		public override void SetStaticDefaults() {
-			this.DisplayName.SetDefault( "" );
+			this.DisplayName.SetDefault( "A Prop" );
 		}
 
 		/// @private
 		public override void SetDefaults() {
+			this.npc.aiStyle = 0;
 			this.npc.width = 16;
 			this.npc.height = 16;
-			this.npc.aiStyle = 0;
 			this.npc.damage = 0;
 			this.npc.defense = 0;
 			this.npc.lifeMax = 5;
@@ -66,18 +71,59 @@ namespace HamstarHelpers.NPCs {
 
 		////////////////
 
+		/// @private
+		public override bool CheckActive() {
+			return false;
+		}
+
+
+		////////////////
+
+		/// @private
+		public override void FindFrame( int frameHeight ) {
+			this.npc.frame = new Rectangle( 0, 0, this.npc.width, this.npc.height );
+		}
+
+
+		////////////////
+
+		/// @private
+		public override bool PreDraw( SpriteBatch sb, Color drawColor ) {
+			Texture2D tex = ModContent.GetTexture( this.Texture );
+			Vector2 pos = UIZoomHelpers.ConvertToScreenPosition( npc.Center, null, true );
+
+			sb.Draw(
+				texture: tex,
+				position: pos,
+				sourceRectangle: null,
+				color: drawColor,
+				rotation: npc.rotation,
+				origin: new Vector2( tex.Width / 2, tex.Height / 2 ),
+				scale: npc.scale,
+				effects: npc.direction == 1 ? SpriteEffects.None : SpriteEffects.FlipHorizontally,
+				layerDepth: 0f
+			);
+
+			if( ModHelpersConfig.Instance.DebugModeMiscInfo ) {
+				var rect = new Rectangle(
+					(int)( pos.X - ((npc.scale * (float)tex.Width) / 2f) ),
+					(int)( pos.Y - ((npc.scale * (float)tex.Height) / 2f) ),
+					(int)( npc.scale * (float)tex.Width ),
+					(int)( npc.scale * (float)tex.Height )
+				);
+				DrawHelpers.DrawBorderedRect( sb, Color.Transparent, Color.Red, rect, 2 );
+			}
+
+			return false;
+		}
+
+
+		////////////////
+
 		/// <summary></summary>
 		/// <param name="texturePath"></param>
-		/// <param name="width"></param>
-		/// <param name="height"></param>
-		public void SetTexture( string texturePath, int? width=null, int? height=null ) {
+		public void SetTexture( string texturePath ) {
 			this._Texture = texturePath;
-			if( width.HasValue ) {
-				this.npc.width = width.Value;
-			}
-			if( height.HasValue ) {
-				this.npc.height = height.Value;
-			}
 		}
 	}
 }
