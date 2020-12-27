3283b18ce23c1aad40909655c1a143f25dc8586f
hamstar0 <webutek@gmail.com>
2020-12-26 19:34:49 -0800

    v5.9.7 - Added ItemMoneyHelpers.RenderMoneyDenominations(int, bool, bool) * Obsoleted ItemMoneyHelpers.RenderMoney(bool) * Added ItemMoneyHelpers.RenderMoneyDenominations(int, bool, bool)

--- HamstarHelpers/Helpers/Items/ItemMoneyHelpers.cs
+++ HamstarHelpers/Helpers/Items/ItemMoneyHelpers.cs
@@ -1,15 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.Localization;
 using HamstarHelpers.Helpers.Players;
+using HamstarHelpers.Helpers.Misc;
 
 
 namespace HamstarHelpers.Helpers.Items {
 	/// <summary>
 	/// Assorted static "helper" functions pertaining to money items.
 	/// </summary>
-	public class ItemMoneyHelpers {
+	public partial class ItemMoneyHelpers {
 		/// <summary></summary>
 		public static Color PlatinumCoinColor { get; } = new Color( 220, 220, 198 );
 
@@ -30,18 +32,14 @@ namespace HamstarHelpers.Helpers.Items {
 		/// Generates an English-formatted string indicating an amount of money.
 		/// </summary>
 		/// <param name="money"></param>
+		/// <param name="addDenom"></param>
+		/// <param name="addColors"></param>
 		/// <returns></returns>
-		public static string RenderMoney( int money ) {
-			string render = "";
-			string labelCopper = Language.GetTextValue( "CopperCoin" );    //Lang.inter[18];
-			string labelSilver = Language.GetTextValue( "SilverCoin" );    //Lang.inter[17];
-			string labelGold = Language.GetTextValue( "GoldCoin" );  //Lang.inter[16];
-			string labelPlat = Language.GetTextValue( "PlatinumCoin" );  //Lang.inter[15];
-
-			int plat = 0;
-			int gold = 0;
-			int silver = 0;
-			int copper = 0;
+		public static string[] RenderMoneyDenominations( long money, bool addDenom, bool addColors ) {
+			long plat = 0;
+			long gold = 0;
+			long silver = 0;
+			long copper = 0;
 
 			if( money < 0 ) { money = 0; }
 
@@ -61,15 +59,54 @@ namespace HamstarHelpers.Helpers.Items {
 				copper = money;
 			}
 
-			if( plat > 0 ) { render += plat + " " + labelPlat; }
-			if( render.Length > 0 ) { render += " "; }
-			if( gold > 0 ) { render += gold + " " + labelGold; }
-			if( render.Length > 0 ) { render += " "; }
-			if( silver > 0 ) { render += silver + " " + labelSilver; }
-			if( render.Length > 0 ) { render += " "; }
-			if( copper > 0 ) { render += copper + " " + labelCopper; }
+			var rendered = new List<string>( 4 );
 
-			return render;
+			if( copper > 0 ) {
+				string render = copper.ToString();
+				if( addDenom ) {
+					render += " " + Language.GetTextValue( "CopperCoin" );    //Lang.inter[18];
+				}
+				if( addColors ) {
+					string colorHex = MiscHelpers.RenderColorHex( ItemMoneyHelpers.CopperCoinColor );
+					render = "[c/"+colorHex+":"+render+"]";
+				}
+				rendered.Add( render );
+			}
+			if( silver > 0 ) {
+				string render = silver.ToString();
+				if( addDenom ) {
+					render += " " + Language.GetTextValue( "SilverCoin" );    //Lang.inter[17];
+				}
+				if( addColors ) {
+					string colorHex = MiscHelpers.RenderColorHex( ItemMoneyHelpers.SilverCoinColor );
+					render = "[c/"+colorHex+":"+render+"]";
+				}
+				rendered.Add( render );
+			}
+			if( gold > 0 ) {
+				string render = gold.ToString();
+				if( addDenom ) {
+					render += " " + Language.GetTextValue( "GoldCoin" );    //Lang.inter[16];
+				}
+				if( addColors ) {
+					string colorHex = MiscHelpers.RenderColorHex( ItemMoneyHelpers.GoldCoinColor );
+					render = "[c/"+colorHex+":"+render+"]";
+				}
+				rendered.Add( render );
+			}
+			if( plat > 0 ) {
+				string render = plat.ToString();
+				if( addDenom ) {
+					render += " " + Language.GetTextValue( "PlatinumCoin" );    //Lang.inter[15];
+				}
+				if( addColors ) {
+					string colorHex = MiscHelpers.RenderColorHex( ItemMoneyHelpers.PlatinumCoinColor );
+					render = "[c/"+colorHex+":"+render+"]";
+				}
+				rendered.Add( render );
+			}
+
+			return rendered.ToArray();
 		}
 
 
---
+++ HamstarHelpers/Helpers/Items/Obsolete/ItemMoneyHelpers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Localization;
+using HamstarHelpers.Helpers.Players;
+
+
+namespace HamstarHelpers.Helpers.Items {
+	/// <summary>
+	/// Assorted static "helper" functions pertaining to money items.
+	/// </summary>
+	public partial class ItemMoneyHelpers {
+		/// @private
+		[Obsolete("use alt", true)]
+		public static string RenderMoney( int money ) {
+			return string.Join( " ", ItemMoneyHelpers.RenderMoneyDenominations(money, true, false) );
+		}
+	}
+}
